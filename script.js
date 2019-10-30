be234a1111b5bd6a0f8efb342311e159a548e108
Gamundi Christian <20138@columbushs.com>
2019-10-30 10:28:47 -0400

    almost done

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 $(document).ready(function() {
-    $('button').click(function(){
-        if ($('body').hasClass('black')) {
-            $('body').removeClass('black').addClass('white');
-        }
-        else {
-            $('body').removeClass('white').addClass('black');
-        }
-    });
-});
-
-if ($('#moonspot1').hasClass('visible')) {
-    $('#moonspot1').removeClass('visible');
-} else {
-    $('#moonspot1').addClass('visible');
-}
+        $('#button').click(function(){
+            if ($('body').hasClass('black')) {
+                $('body').removeClass('black').addClass('white');
+            }
+            else {
+                $('body').removeClass('white').addClass('black');
+            }
+        });
+        });
